bab86fde4aee9bc76ad94765862fe2ce603f72b9
DaPigGuy <mcpepig123@gmail.com>
2023-09-05 19:02:53 -0700

    Port to GNOME 45

--- unredirect@vaina.lt/extension.js
+++ unredirect@vaina.lt/extension.js
@@ -1,24 +1,11 @@
-const Meta = imports.gi.Meta;
+import Meta from 'gi://Meta';
 
-function init() {
-}
-
-function enable() {
-	try {
-		// >= 3.30
+export default class Extension {
+	enable() {
 		Meta.disable_unredirect_for_display(global.display);
-	} catch (_ex) {
-		// < 3.30
-		Meta.disable_unredirect_for_screen(global.screen);
 	}
-}
 
-function disable() {
-	try {
-		// >= 3.30
+	disable() {
 		Meta.enable_unredirect_for_display(global.display);
-	} catch (_ex) {
-		// < 3.30
-		Meta.enable_unredirect_for_screen(global.screen);
 	}
 }
