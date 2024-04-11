1bdd98f3d13614df97237a4bb65e324d90529349
Kazimieras Vaina <kazimieras.vaina@gmail.com>
2024-04-11 21:34:50 +0300

    feat: resort to a monkey patch (#33)
    feat: resort to a monkey patch

--- unredirect@vaina.lt/extension.js
+++ unredirect@vaina.lt/extension.js
@@ -2,20 +2,28 @@ import Meta from 'gi://Meta';
 
 export default class UnredirectExtension {
     constructor() {
-        this.isEnabled = false;
+        this.orig_enable_unredirect_for_display = null;
     }
 
     enable() {
-        if (!this.isEnabled) {
+        if (this.orig_enable_unredirect_for_display == null) {
+            console.debug("Monkey patching Meta.enable_unredirect_for_display(display) function");
+            this.orig_enable_unredirect_for_display = Meta.enable_unredirect_for_display;
+            Meta.enable_unredirect_for_display = function(display) {
+                // should I check for display?
+                console.debug("Ignoring invocation of Meta.enable_unredirect_for_display(display)");
+            };
             Meta.disable_unredirect_for_display(global.display);
-            this.isEnabled = true;
         }
     }
 
     disable() {
-        if (this.isEnabled) {
+        if (this.orig_enable_unredirect_for_display != null) {
+            console.debug("Restoring Meta.enable_unredirect_for_display(display) function");
+            Meta.enable_unredirect_for_display = this.orig_enable_unredirect_for_display;
+            this.orig_enable_unredirect_for_display = null;
+            console.debug("Invoking Meta.enable_unredirect_for_display(display)");
             Meta.enable_unredirect_for_display(global.display);
-            this.isEnabled = false;
         }
     }
 }
